2bc143003b0bf2497cab0c820940871e6e621c1b
Ei <goei3524@gmail.com>
2020-02-13 17:06:55 +0800

    init

--- filter.js
+++ filter.js
@@ -5,5 +5,9 @@ module.exports = {
       'src/projects/projectName/pages/pageName/index.scss',
       'src/projects/projectName/var.scss'
   ],
-  project: ['pageName']
+  project: [
+    'src/projects/projectName/pages/pageName/index.html',
+    'src/projects/projectName/pages/pageName/index.js',
+    'src/projects/projectName/pages/pageName/index.scss'
+  ]
 };

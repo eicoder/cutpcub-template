791dc6a91feabc1fbe220a358b56e69586f30cb4
guoyiqiang <guoyqiang@foxmail.com>
2020-02-19 09:49:00 +0800

    模版更新

--- filter.js
+++ filter.js
@@ -4,7 +4,8 @@ module.exports = {
       'src/projects/projectName/pages/pageName/index.js',
       'src/projects/projectName/pages/pageName/index.scss',
       'src/projects/projectName/var.scss',
-      'src/assets/projects/projectName/images/cub.png'
+      'src/assets/projects/projectName/images/cub.png',
+      'src/projects/projectName/sprites/sprites.css'
   ],
   project: [
     'src/projects/projectName/pages/pageName/index.html',

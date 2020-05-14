121cb7ad9e695249ff2738f8bafd34ef70b4f835
Ei <goei3524@gmail.com>
2020-05-14 20:13:29 +0800

    init

--- template/src/projects/projectName/pages/pageName/index.js
+++ template/src/projects/projectName/pages/pageName/index.js
@@ -1,14 +1,2 @@
 import '@/styles/index.scss';
 import './index.scss';
-
-new Vue({
-  el: '#app',
-  data: {
-    visible: false
-  },
-  methods: {
-    show: function () {
-      this.visible = true;
-    }
-  }
-});
